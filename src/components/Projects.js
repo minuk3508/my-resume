c2c7ef60db446bac8cdcf26e273e352f5623de8b
김민욱 <minuk3508@gmail.com>
2022-11-30 11:22:31 +0900

    공커밋

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -75,22 +75,10 @@ export const Projects = () => {
                       </Row>
                     </Tab.Pane>
                     <Tab.Pane eventKey="section">
-                      <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Cumque quam, quod neque provident velit, rem explicabo
-                        excepturi id illo molestiae blanditiis, eligendi dicta
-                        officiis asperiores delectus quasi inventore debitis
-                        quo.
-                      </p>
+                      <p>준비중입니다.</p>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
-                      <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Cumque quam, quod neque provident velit, rem explicabo
-                        excepturi id illo molestiae blanditiis, eligendi dicta
-                        officiis asperiores delectus quasi inventore debitis
-                        quo.
-                      </p>
+                      <p>준비중입니다.</p>
                     </Tab.Pane>
                   </Tab.Content>
                 </Tab.Container>
